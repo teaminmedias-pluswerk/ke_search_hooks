4d15965300af910ab9f8ba95e5769c58dd7ec600
Christian Bülter <christian.buelter@inmedias.de>
2019-06-21 19:09:03 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Faceted Search Hooks Example',
     'description' => 'Hooks example for ke_search. Feel free to use this as a kickstarter for your own custom indexer or hooks. Implements a news indexer as example.',
     'category' => 'backend',
-    'version' => '2.0.0',
+    'version' => '3.0.0',
     'dependencies' => 'ke_search',
     'state' => 'stable',
     'author' => 'Christian Buelter',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'team.inmedias',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '9.5.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
